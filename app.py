a2a79feb6594b6755cc2da11bc4c46c7f5c94ce9
Ideal Robot <theidealrobot@gmail.com>
2026-06-11 20:51:35 -0600

    Updated route

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("hobbies")
+@app.route("/hobbies")
 def hobbies():
     return render_template("hobbies.html")
 
