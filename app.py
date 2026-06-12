11b36cb9027914e5b30273ad27226ab084b16f33
Ideal Robot <theidealrobot@gmail.com>
2026-06-11 20:50:57 -0600

    Creating my hobbies.html page... and created a route so we can find the damn thing

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("hobbies")
+def hobbies():
+    return render_template("hobbies.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
